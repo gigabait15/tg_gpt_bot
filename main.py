cc266a73928c8bc7135fee719e5251950280eff2
Evgeniy <evgenii.kerbecov@fe-conn.com>
2026-02-11 13:21:57 +0800

    add web ping

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-
+from aiohttp import web
 from core.settings import settings
 from core.database.db import db_session
 from src.handlers import router
@@ -23,5 +23,17 @@ async def main():
         await db_session.close()
 
 
+async def handle(request):
+    return web.Response(text="I am alive")
+
+async def run_web_server():
+    app = web.Application()
+    app.add_routes([web.get('/', handle)])
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner, '0.0.0.0', port=8080)
+    await site.start()
+
+asyncio.create_task(run_web_server())
 if __name__ == "__main__":
     asyncio.run(main())
